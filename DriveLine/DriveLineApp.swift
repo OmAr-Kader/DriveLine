a155707d8a4d976d34761155562a22f0eb651478
Omar Kader <ramooor700@gmail.com>
2025-11-06 21:54:08 +0200

    Initial Commit

---
+++ DriveLine/DriveLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveLineApp.swift
+//  DriveLine
+//
+//  Created by OmAr Kader on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DriveLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
